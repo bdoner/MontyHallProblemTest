7896c13defecec55becb0debd87574d4ca32edf3
U-Nikolaj-HP\Nikolaj <Nikolaj@Nikolaj-HP.(none)>
2013-08-27 06:57:32 +0200

    initial commit

---
+++ montyUtils.py
@@ -0,0 +1,70 @@
+import random
+
+__author__ = 'Nikolaj'
+
+class DoorState:
+    Closed = 0
+    Open = 1
+
+class Door:
+    State = DoorState.Closed
+    HasPrize = False
+    CurrentGuess = False
+
+    def __init__(self):
+        self.State = DoorState.Closed
+        self.HasPrize = False
+        self.CurrentGuess = False
+
+class DoorSet:
+    Door1 = Door()
+    Door2 = Door()
+    Door3 = Door()
+    __doors = [Door1,Door2,Door3]
+
+    def get_rnd(self):
+        return random.choice([0,1,2])
+
+    def __init__(self):
+        Door1 = Door()
+        Door2 = Door()
+        Door3 = Door()
+        __doors = [self.Door1, self.Door2, self.Door3]
+
+        for d in range(3):
+            self.__doors[d].CurrentGuess = False
+            self.__doors[d].HasPrize = False
+            self.__doors[d].State = DoorState.Closed
+
+        winningdoor = self.get_rnd();
+        self.__doors[winningdoor].HasPrize = True
+
+
+    def place_guess(self, pos):
+        for d in range(3):
+            self.__doors[d].CurrentGuess = False
+        self.__doors[pos].CurrentGuess = True
+
+    def check_guess(self):
+        for d in range(3):
+            if self.__doors[d].CurrentGuess:
+                return self.__doors[d].HasPrize
+
+    def open_nonprize_door(self):
+        rnddoor = self.get_rnd()
+        while True:
+            if not self.__doors[rnddoor].HasPrize and not self.__doors[rnddoor].CurrentGuess:
+                self.__doors[rnddoor].State = DoorState.Open
+                break
+            else:
+                rnddoor = self.get_rnd()
+
+    def change_guess(self):
+        pos = -1
+        for d in range(3):
+            if not self.__doors[d].CurrentGuess:
+                if self.__doors[d].State == DoorState.Closed:
+                    pos = d
+        for d in range(3):
+            self.__doors[d].CurrentGuess = False
+        self.__doors[pos].CurrentGuess = True
---
+++ montyhall.py
@@ -0,0 +1,60 @@
+from montyUtils import *
+import random
+__author__ = 'Nikolaj'
+
+numTests = 100000000
+totalFirst = 0
+totalSecond = 0
+totalGuesses = 0
+winFirst = 0
+lostFirst = 0
+winSecond = 0
+lostSecond = 0
+def collect_info(firstGuess, correctGuess):
+    global totalGuesses
+    global totalFirst
+    global totalSecond
+    global winFirst
+    global lostFirst
+    global winSecond
+    global lostSecond
+
+    totalGuesses += 1
+
+    if firstGuess and correctGuess:
+        winFirst = winFirst + 1
+        totalFirst += 1
+    elif firstGuess and not correctGuess:
+        lostFirst = lostFirst + 1
+        totalFirst += 1
+    elif not firstGuess and correctGuess:
+        winSecond = winSecond + 1
+        totalSecond += 1
+    elif not firstGuess and not correctGuess:
+        lostSecond = lostSecond + 1
+        totalSecond += 1
+
+    if totalGuesses == numTests:
+
+        print('\n-------------------------------\n#' + str(totalGuesses))
+        print(str((winFirst / totalFirst) * 100.0) + "% wins on first guess")
+        print(str((lostFirst / totalFirst) * 100.0) + "% losses on first guess")
+        print('\n')
+        print(str((winSecond / totalSecond) * 100.0) + "% wins on second guess")
+        print(str((lostSecond / totalSecond) * 100.0) + "% losses on second guess")
+
+for i in range(numTests):
+    doorSet = DoorSet()
+
+    firstGuess = doorSet.get_rnd()
+    doorSet.place_guess(firstGuess)
+    doorSet.open_nonprize_door()
+
+    choise = totalGuesses % 2 == 0 # random.choice([True,False])
+    if choise:
+        doorSet.change_guess()
+        win = doorSet.check_guess()
+        collect_info(False, win)
+    else:
+        win = doorSet.check_guess()
+        collect_info(True, win)
